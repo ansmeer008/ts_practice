2b55a6198367049dcf78f45929048e9f893808fb
ansmeer008 <ansmeer008@gmail.com>
2023-03-14 12:08:00 +0900

    형 변환 학습

--- advanced/app.js
+++ advanced/app.js
@@ -1,11 +1,11 @@
 "use strict";
-//intersection type : 다른 타입을 결합할 수 있다.
+//***********************intersection type : 다른 타입을 결합할 수 있다.***********************/
 const e1 = {
     name: "Max",
     priviliges: ["create-server"],
     startDate: new Date(),
 };
-//타입 가드
+//***********************타입 가드***********************/
 // Combinabl과 같은 유니언 타입은 유연성을 가지지만 런타임 시 정확히 어떤 타입을 가질지 알아야 하는 경우들이 있음.
 function addthings(a, b) {
     //타입 가드
@@ -28,7 +28,7 @@ function printEmployeeInformation(emp) {
 }
 printEmployeeInformation(e1);
 printEmployeeInformation({ name: "Nana", startDate: new Date() });
-//클래스에서 타입가드 사용하기
+//***********************클래스에서 타입가드 사용하기***********************/
 class Car {
     drive() {
         console.log("Driving...");
@@ -68,3 +68,30 @@ function moveAnimals(animal) {
     console.log("Moving at speed: " + speed);
 }
 moveAnimals({ type: "bird", flyingSpeed: 30 });
+/***********************형 변환***********************/
+//:타입스크립트가 직접 감지하지 못하는 특정 타입의 값을 타입스크립트에 알려주는 역할
+//아래와 같이 작성하면 타입스크립트는 해당 요소가 p 요소임을 알지만
+// const paragraph = document.querySelector("p");
+//아래와 같이 작성하면 그냥 html 요소라고 밖에 인식하지 못한다.
+// const paragraph = document.getElementById("message-output");
+// const userInputElement = document.getElementById("user-input");
+//아래 코드는 에러가 발생할 수 있음. 일단 userInputElement가 null일 수 있고,
+//단순히 html 요소라는 것만으로는 해당 요소에ㅐ value라는 속성이 있는지 알 수 없음.
+// userInputElement.value = 'Hi there!';
+//형변환 사용해보기 : 아래처럼 작성하면 에러가 뜨지 않음.
+//1. 앞쪽에 추가하기
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")
+// );
+// userInputElement.value = "Hi there";
+//2.뒤쪽에 as 추가하기
+// const userInputElement = document.getElementById(
+//   "user-input"
+// )! as HTMLInputElement;
+// userInputElement.value = "Hi there";
+//느낌표는 null을 반환하지 않을 것이라는 뜻, 느낌표 대신 if로 userInputElement가 존재하는지 체크해줄 수도 있음.
+//대신 if문을 사용하려면 형 변환을 null 체크 이후로 해야함.
+const userInputElement = document.getElementById("user-input");
+if (userInputElement) {
+    userInputElement.value = "Hi there";
+}
--- advanced/app.ts
+++ advanced/app.ts
@@ -1,4 +1,4 @@
-//intersection type : 다른 타입을 결합할 수 있다.
+//***********************intersection type : 다른 타입을 결합할 수 있다.***********************/
 
 type Admin = {
   name: string;
@@ -26,7 +26,7 @@ type Numeric = number | boolean;
 //유니언 타입인 Combinabl과 Nummeric의 교차점이 number 타입이기 때문에.
 type Universal = Combinabl & Numeric;
 
-//타입 가드
+//***********************타입 가드***********************/
 // Combinabl과 같은 유니언 타입은 유연성을 가지지만 런타임 시 정확히 어떤 타입을 가질지 알아야 하는 경우들이 있음.
 function addthings(a: Combinabl, b: Combinabl) {
   //타입 가드
@@ -56,7 +56,7 @@ function printEmployeeInformation(emp: UnknownEmployee) {
 printEmployeeInformation(e1);
 printEmployeeInformation({ name: "Nana", startDate: new Date() });
 
-//클래스에서 타입가드 사용하기
+//***********************클래스에서 타입가드 사용하기***********************/
 
 class Car {
   drive() {
@@ -95,7 +95,7 @@ function useVehicle(vehicle: Vehicle) {
 useVehicle(v1);
 useVehicle(v2);
 
-//구별된 유니언
+//***********************구별된 유니언***********************/
 //구별된 유니언은 아래와 같이 유니언을 구성하는 모든 객체에 하나의 공통 속성만 있고
 //해당 속성이 유니언을 설명하므로, 이 속성을 switch문에 사용해 타입 안전성을 갖추고 객체에 어떤 속성을 사용할 수 있는지 파악 가능.
 
@@ -124,3 +124,39 @@ function moveAnimals(animal: Animals) {
 }
 
 moveAnimals({ type: "bird", flyingSpeed: 30 });
+
+/***********************형 변환***********************/
+//:타입스크립트가 직접 감지하지 못하는 특정 타입의 값을 타입스크립트에 알려주는 역할
+
+//아래와 같이 작성하면 타입스크립트는 해당 요소가 p 요소임을 알지만
+// const paragraph = document.querySelector("p");
+//아래와 같이 작성하면 그냥 html 요소라고 밖에 인식하지 못한다.
+// const paragraph = document.getElementById("message-output");
+
+// const userInputElement = document.getElementById("user-input");
+
+//아래 코드는 에러가 발생할 수 있음. 일단 userInputElement가 null일 수 있고,
+//단순히 html 요소라는 것만으로는 해당 요소에ㅐ value라는 속성이 있는지 알 수 없음.
+// userInputElement.value = 'Hi there!';
+
+//형변환 사용해보기 : 아래처럼 작성하면 에러가 뜨지 않음.
+//1. 앞쪽에 추가하기
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")
+// );
+// userInputElement.value = "Hi there";
+
+//2.뒤쪽에 as 추가하기
+// const userInputElement = document.getElementById(
+//   "user-input"
+// )! as HTMLInputElement;
+
+// userInputElement.value = "Hi there";
+
+//느낌표는 null을 반환하지 않을 것이라는 뜻, 느낌표 대신 if로 userInputElement가 존재하는지 체크해줄 수도 있음.
+//대신 if문을 사용하려면 형 변환을 null 체크 이후로 해야함.
+const userInputElement = document.getElementById("user-input");
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there";
+}
