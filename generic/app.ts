0db85a4dd7f95505f6c93c22964b803a8838b893
ansmeer008 <ansmeer008@gmail.com>
2023-04-09 15:36:23 +0900

    generic class

--- generic/app.ts
+++ generic/app.ts
@@ -1,46 +1,40 @@
-//제네릭 함수 생성
-// function merge(A: object, B: object) {
-//   return Object.assign(A, B);
-// }
+//제네릭 클래스
 
-// const mergeObj = merge({name:'Max'},{age:30});
-//아래와 같이 접근할 수 없다.
-// mergeObj.name
+//아래 DataStorage 클래스는 item의 일관성만 존재하는 제네릭 클래스로 만들고자 했으므로
+//DataStorae에 T라는 데이터 타입 식별자를 명시해주고, 그 아래 데이터들에도 T를 적용해준다.
+//이 클래스는 원시 타입하고만 작동해야 편한데 그 이유는 아래쪽 ObjStorage 예시 확인
+class DataStorage<T extends string | boolean | number> {
+  private data: T[] = [];
 
-//제네릭 함수로 바꾸면 해결할 수 있다.
-//아래와 같이 작성하면 타입스크립트는 merge 함수가 T와 U의 인터섹션을 반환한다고 추론함.
-//extends와 같은 제약 조건을 걸어주면 함수의 기능을 개선할 수 있음.
-//제약 조건을 걸어주지 않았을 때는 ObjectB에 숫자가 할당되었더라도 내부에서만 실패하고
-//아무런 에러를 띄우지 않음. 제약 조건을 걸어주면 에러 발생.
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
-}
-
-const mergedObj = merge({ name: "Max" }, { age: 30 });
-console.log(mergedObj.age);
-
-//단순히 T를 타입으로 지정해주었을 때는 아래 element.length라는 부분에서 에러가 떴음.
-//Lengthy라는 length 속성을 가진 인터페이스를 생성해주고 이를 T에 extends해주면
-//아래 함수에 어떤 인자가 들어오든지 length 속성을 가진다는 것을 타입스크립트에게 알려줄 수 있음.
-interface Lengthy {
-  length: number;
-}
-
-//타입스크립트는 아래 함수가 문자열이나 T타입 요소를 지니는 배열이 반환된다고 추론한다.
-//튜플을 반환한다고 정확히 알려주기 위해 반환 타입을 추가해준다.
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let descriptionText = "Got no value.";
-  if (element.length === 1) {
-    descriptionText = "Got 1 elements.";
-  } else if (element.length > 1) {
-    descriptionText = "Got " + element.length + " elements.";
+  addItem(item: T) {
+    this.data.push(item);
   }
-  return [element, descriptionText];
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
 }
 
-console.log(countAndDescribe("Hi there!"));
-console.log(countAndDescribe(["Sports", "Cooking"]));
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
 
-//이전에 학습했던 함수 타입 지정과 크게 다르지는 않지만,
-//제네릭 타입을 이용해서 좀 더 유연한 타입 지정을 할 수 있음
-//ex 문자열이나 배열로 element를 제한하고 싶지는 않지만 length 속성은 필수로 가질 수 있도록
+//만약 객체가 담기는 DataStorage의 인스턴스를 만든다면
+//객체는 참조 자료형이기 때문에 removeItem 매서드 로직에서 문제가 발생한다
+//주소값으로 indexOf가 제대로 작동하지 않으면 -1을 반환하므로
+//가장 마지막의 요소를 제거하게 되므로 의도한 대로 작동하지 않게 됨
+//이를 해결하기 위해 this.data.indexOf가 제대로 동작했는지 확인하는 구문을 넣어줄 수 잇음.
+//아니면 addItem으로 특정 객체를 전달할 때 const maxObj = {name: "Max"};와 같이
+//변수로 만든 객체를 전달해줄 수도 있다.
+//그러면 removeItem을 할 때도 해당 변수를 전달해주어 동일 주소값을 참조 가능.
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: "Max" });
+// objStorage.addItem({ name: "Manu" });
+// objStorage.removeItem({ name: "Max" });
+// console.log(objStorage.getItems());
